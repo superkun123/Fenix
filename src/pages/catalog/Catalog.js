ed25fc730d741c380031ee11b37e81f2dee87b45
Egor Kolyadintsev <kolyadintsev1997@mail.ru>
2021-03-26 15:20:55 +0500

    добавил количество имён

--- src/pages/catalog/Catalog.js
+++ src/pages/catalog/Catalog.js
@@ -314,8 +314,7 @@ onPress={() => navigation.navigate('ProfileScreen', {
 
 <View style={styles.header}>
       <Text  style={styles.title}>Каталог имен</Text>
-      <Text  style={styles.subtitle}>323 имени</Text>
-      <Text>{route.params.male}</Text>
+      <Text  style={styles.subtitle}>{data.length} имени</Text>
       <Pressable  onPress={() => navigation.navigate('Filter')}  style={styles.settings}>
       <Ionicons name="ios-settings-outline" size={24} color="black" />
       </Pressable>

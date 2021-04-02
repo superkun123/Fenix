05e22d347f5b30a756dce0577ed2332f4d1a288a
Egor Kolyadintsev <kolyadintsev1997@mail.ru>
2021-04-02 16:58:51 +0500

    Добавил стрелку

--- src/pages/gender/Gender.js
+++ src/pages/gender/Gender.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Pressable, ScrollView, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Pressable, ScrollView, Button, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -12,6 +12,7 @@ import { SvgComponentUnknown } from '../../../assets/jsxSvg/unknownIcon'
 import { SvgComponentArrowRight } from '../../../assets/jsxSvg/arrowRightWhite'
 import { Feather } from '@expo/vector-icons'; 
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { AntDesign } from '@expo/vector-icons'; 
 
 
 
@@ -73,16 +74,18 @@ export  function Gender() {
   
       return (
         <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}}>
-        <Stack.Screen name="GenderScreen" component={GenderScreen}  screenOptions={{
-    headerBackTitleVisible: false,
-
-  }}
-   options={{ title: 'Пол ребенка', headerStyle: {
+        <Stack.Screen name="GenderScreen" component={GenderScreen}  
+   options={{ title: 'Пол ребенка',  headerStyle: {
             shadowOpacity: 0,
             elevation: 0
           },
+          headerTintColor: "#5DADC1",
+          headerBackImage: () =>  (<SvgComponentArrowRight style={styles.navArrow} color='#000'></SvgComponentArrowRight>),
+          headerBackTitle: () => null,
+          headerBackTitleVisible: false,
           headerTitleStyle: {
             fontFamily: 'GilroyMedium',
+            color: '#444444'
           },
           } }  />
         {/* <Stack.Screen name="FatherName" component={FatherName}  options={{ title: 'Имя отца', headerStyle: {
@@ -94,7 +97,14 @@ export  function Gender() {
           },
           }  }/> */}
 
-      <Stack.Screen name="FatherName" component={FatherName} options={{headerShown: false}}   />
+      <Stack.Screen name="FatherName" component={FatherName}   options={{
+        headerTintColor: "#5DADC1",
+    // headerBackTitleVisible: false,
+    // headerBackTitle: () => null,
+    // headerBackImage: () =>  (<Image source={require('../../../assets/icons/prev.png')} />),
+
+
+  }} options={{headerShown: false}}   />
 
       </Stack.Navigator>
       );
@@ -119,6 +129,11 @@ export  function Gender() {
           width: '100%',
           alignContent: 'center',
           justifyContent: 'center'
+      },
+      navArrow: {
+        transform: [{rotate: '180deg'}],
+        marginLeft: 10
+
       },
       svgGenger: {
           marginBottom: 20,
